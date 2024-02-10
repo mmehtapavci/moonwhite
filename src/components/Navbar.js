7750a4f91b5578bf6c21936cd54329253f6ba58e
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-10 14:58:37 +0300

    Edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import './style/Navbar.css';
+import '../style/Navbar.css';
 
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" >MoonWhite</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link" >Features</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
+                        <a className="nav-link" >Pricing</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a className="nav-link disabled" tabindex="-1" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
             </div>
