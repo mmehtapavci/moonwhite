3690c4b190ea91314fb692d31555437f07cb6ad0
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-10 13:56:52 +0300

    The React project file has been created

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import '../style/App.css';
+
+function App() {
+    return (
+        <h1>selam</h1>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import Navbar from './components/Navbar'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <>
+        <Navbar />
+        <App />
+    </>
+);
+
