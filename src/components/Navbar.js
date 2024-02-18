530aab9604e39a3caeb40913a29aa872569ca463
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-18 20:44:35 +0300

    Navbar edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ import '../style/Navbar.css';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" >MoonWhite</a>
+            <a className="navbar-brand" href="#">MoonWhite</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" >Features</a>
+                        <a className="nav-link" href="#">Features</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" >Pricing</a>
+                        <a className="nav-link" href="#">Pricing</a>
                     </li>
                 </ul>
             </div>
