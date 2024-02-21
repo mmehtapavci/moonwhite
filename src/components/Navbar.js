3d70fd65a209d3aa485c4936e9052eb9f4f37d5d
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-21 22:24:47 +0300

    Edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import '../style/Navbar.css';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">MoonWhite</a>
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <a className="navbar-brand" >MoonWhite</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../style/Section.css';
 
-function Section({ image, text }) {
+function Section({ image }) {
     return (
         <div className="section">
-            <img src={image} alt="Coffee" className="section-image" />
+            <div className="content">
+                <div className="parent">
+                    <h2 className='coffeabout' >MoonWhite Coffe </h2>
+                    <p className='about' >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for their default model text, and a search for their default model text, and a search for search for their default model text, and search for their default model
+                    </p>
+                </div>
+                <img src={image} alt="Coffee" className="section-image" />
+            </div>
         </div>
     );
 }
