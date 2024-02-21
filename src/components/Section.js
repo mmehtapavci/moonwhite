9693c320efc09489fce37d73e8e08b917237defc
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-21 14:53:01 +0300

    İmage edit

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../style/Section.css';
 
-function Section({ image }) {
+function Section({ image, text }) {
     return (
-        <div className="section image">
-            <img src={image} alt="Coffe" />
+        <div className="section">
+            <img src={image} alt="Coffee" className="section-image" />
         </div>
     );
 }
