9c60faa5990be3ec290f2df3ea7ebc3070d8356f
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-24 14:27:48 +0300

    imgeditt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
+// App.js
+import React from 'react';
 import '../style/App.css';
 import Navbar from './Navbar';
 import Section from './Section';
 import SectionTwo from './SectionTwo';
 import SectionImage from '../img/Bloom-3.jpg';
-import SectionTwoImage from '../img/coff.jpg'
-
-
+import SectionTwoImage from '../img/coff.jpg';
+import SectionTwoImage1 from '../img/boximage1.jpg';
+import SectionTwoImage2 from '../img/boximage2.jpg';
+import SectionTwoImage3 from '../img/boximage3.jpg';
+import SectionTwoImage4 from '../img/boximage4.jpg';
 
 function App() {
     return (
@@ -14,7 +18,13 @@ function App() {
             <Section
                 image={SectionImage}
             />
-            <SectionTwo background={SectionTwoImage} />
+            <SectionTwo
+                background={SectionTwoImage}
+                boximage1={SectionTwoImage1}
+                boximage2={SectionTwoImage2}
+                boximage3={SectionTwoImage3}
+                boximage4={SectionTwoImage4}
+            />
         </>
     );
 }
--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -1,16 +1,17 @@
+// SectionTwo.js
 import React from 'react';
 import '../style/SectionTwo.css';
 
-function SectionTwo({ background }) {
+function SectionTwo({ background, boximage1, boximage2, boximage3, boximage4 }) { 
     return (
         <>
             <img src={background} alt="background" className="sectiontwo-image" />
             <div className="box-container">
-                <div className="box">eee</div>
-                <div className="box">rrr</div>
-                <div className="box">ttt</div>
-                <div className="box">yyy</div>
-            </div>  
+                <img className='boximg' src={boximage1} alt="" />
+                <img className='boximg' src={boximage2} alt="" />
+                <img className='boximg' src={boximage3} alt="" />
+                <img className='boximg' src={boximage4} alt="" /> 
+            </div>
         </>
     );
 }
