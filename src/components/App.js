dc477d4873624df08bd3cc373fd4a68281479251
mmehtapavci <mmmehtapavci@gmail.com>
2024-02-25 22:21:39 +0300

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,9 @@ import SectionTwo from './SectionTwo';
 import SectionImage from '../img/Bloom-3.jpg';
 import SectionTwoImage from '../img/coff.jpg';
 import SectionTwoImage1 from '../img/boximage1.jpg';
-import SectionTwoImage2 from '../img/boximage2.jpg';
+// import SectionTwoImage2 from '../img/boximage2.jpg';
 import SectionTwoImage3 from '../img/boximage3.jpg';
-import SectionTwoImage4 from '../img/boximage4.jpg';
+// import SectionTwoImage4 from '../img/boximage4.jpg';
 
 function App() {
     return (
@@ -21,9 +21,9 @@ function App() {
             <SectionTwo
                 background={SectionTwoImage}
                 boximage1={SectionTwoImage1}
-                boximage2={SectionTwoImage2}
+                // boximage2={SectionTwoImage2}
                 boximage3={SectionTwoImage3}
-                boximage4={SectionTwoImage4}
+                // boximage4={SectionTwoImage4}
             />
         </>
     );
--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -2,15 +2,24 @@
 import React from 'react';
 import '../style/SectionTwo.css';
 
-function SectionTwo({ background, boximage1, boximage2, boximage3, boximage4 }) { 
+
+
+
+function SectionTwo({ background, boximage1,boximage3, }) { 
     return (
         <>
             <img src={background} alt="background" className="sectiontwo-image" />
             <div className="box-container">
                 <img className='boximg' src={boximage1} alt="" />
-                <img className='boximg' src={boximage2} alt="" />
+                <div className="box">
+                    <p className="box-text">Metin 1</p>
+                </div>
+                {/* <img className='boximg' src={boximage2} alt="" /> */}
                 <img className='boximg' src={boximage3} alt="" />
-                <img className='boximg' src={boximage4} alt="" /> 
+                {/* <img className='boximg' src={boximage4} alt="" />  */}
+                <div className="box">
+                    <p className="box-text">Metin 4</p>
+                </div>
             </div>
         </>
     );
