c91601f9d8c8122a4a9545cdf1b6892d3d717202
mmehtapavci <mmmehtapavci@gmail.com>
2024-03-03 16:57:22 +0300

    editt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import '../style/Navbar.css';
 
 function Navbar() {
+    // Ürünler bağlantısına tıklandığında Sectiontree bileşenine gitmesini sağlayan fonksiyon
+    const goToProducts = () => {
+        // Sectiontree bileşenine geçiş yapmak için gerekli kodlar
+        // Örnek: window.location.href = '/products';
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <a className="navbar-brand" >MoonWhite</a>
@@ -11,7 +17,7 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" >Ürünler </a>
+                        <a className="nav-link" onClick={goToProducts}>Ürünler </a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" >Giriş Yap </a>
--- src/components/Sectiontree.js
+++ src/components/Sectiontree.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
 import '../style/Sectiontree.css';
 
-
-
-
-
 function Sectiontree() {
+    // Örnek ürün listesi
+    const products = [
+        { id: 1, name: "Ürün 1", price: 10 },
+        { id: 2, name: "Ürün 2", price: 15 },
+        { id: 3, name: "Ürün 3", price: 20 },
+        { id: 4, name: "Ürün 4", price: 25 },
+        { id: 5, name: "Ürün 5", price: 30 },
+        { id: 6, name: "Ürün 6", price: 35 }
+    ];
+
     return (
         <div className="products">
+            <h1>Ürünler</h1>
             <ul>
-                <li></li>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.name} - {product.price} TL
+                    </li>
+                ))}
             </ul>
         </div>
-    )
+    );
 }
+
 export default Sectiontree;
