d14c118be699b70952b6464aec5a36ee73c0d279
mmehtapavci <mmmehtapavci@gmail.com>
2024-03-10 18:46:42 +0300

    edit sectiontree card

--- src/components/Sectiontree.js
+++ src/components/Sectiontree.js
@@ -1,32 +1,120 @@
-import React from 'react'
+import React from 'react';
 import '../style/Sectiontree.css';
 import { ListGroup, ListGroupItem } from 'reactstrap';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
-
-
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Container } from 'reactstrap';
 
 function Sectiontree() {
     return (
         <div className="products">
-            <h1>ürünler</h1>
-              <ListGroup>
-                <ListGroupItem>Cras justo odio</ListGroupItem>
-                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                <ListGroupItem>Morbi leo risus</ListGroupItem>
-                <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-              </ListGroup>
-
-              <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
+            <h1>Ürünler</h1>
+            <div className="row">
+                <div className="col-md-3">
+                    <ListGroup>
+                        <ListGroupItem>Cras justo odio</ListGroupItem>
+                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                        <ListGroupItem>Morbi leo risus</ListGroupItem>
+                        <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                    </ListGroup>
+                </div>
+                <div className="col-md-9">
+                    <Container>
+                        <div className="row">
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                            <div className="col-md-3 mb-4">
+                                <Card>
+                                    <CardImg top width="70%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                    <CardBody>
+                                        <CardTitle>Card title</CardTitle>
+                                        <CardSubtitle>Card subtitle</CardSubtitle>
+                                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                        <Button>Button</Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+            </div>
         </div>
     )
 }
+
 export default Sectiontree;
